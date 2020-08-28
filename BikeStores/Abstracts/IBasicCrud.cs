42fe8c8a883f5b61dfc11dfcb9ac726e9d4fc5c0
developer.performate <developer@performate.id>
2020-08-28 14:28:43 +0700

    init

---
+++ BikeStores/Abstracts/IBasicCrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeStores.Abstracts
+{
+    interface IBasicCrud
+    {
+        List<dynamic> fetchAll();//Method to fetch all records
+        int checkPrivilege(string username, string methodName);//to check whether user can fetch data
+        object fetchOne(object searchParam);
+        void insert(object rec);
+        void update(object updateParam, object newrec);
+        void delete(object deleteParam);
+    }
+}
